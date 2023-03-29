486c2662bd4425c9c8267a3ba5adc1b29e930c92
avchirkin <chirkin@dotnet.ru>
2023-03-29 10:15:13 +0300

    Added authentication and authorization

---
+++ LevelUp.CsharpJuniors.Tasks/SimpleStore.UI/Data/UsersDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleStore.UI.Data;
+
+public class UsersDbContext : IdentityDbContext
+{
+    public UsersDbContext(DbContextOptions<UsersDbContext> options)
+        : base(options)
+    {
+    }
+}
--- LevelUp.CsharpJuniors.Tasks/SimpleStore.UI/Program.cs
+++ LevelUp.CsharpJuniors.Tasks/SimpleStore.UI/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using SimpleStore.UI.Areas.Identity;
+using SimpleStore.UI.Data;
 using SimpleStore.UI.Models;
 using SimpleStore.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<UsersDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<UsersDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services
+    .AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 
 var endpoints = builder.Configuration.GetSection("Endpoints").Get<Endpoints>();
 builder.Services.AddHttpClient("default",
@@ -33,6 +46,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
